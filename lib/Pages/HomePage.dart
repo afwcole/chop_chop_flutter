981c8f78509d52471d6d7f9290f791d57706cca0
afwcole <afwcole@gmail.com>
2020-04-21 22:58:57 +0000

    Add Search Bar to Homepage
    Search Bar that allows the user to search through all the mealItems
    available. Click on each ListTile opens Meal Profile Page.

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -22,8 +22,11 @@ class _HomePageState extends State<HomePage> {
         title: Image.asset( "images/Logo.png", height: 45, width: 45),
         centerTitle: true,
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.search),
-            onPressed: () {}),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              showSearch(context: context, delegate: DataSearch());
+            })
         ]
       ),
       drawer: standardDrawer(context),
@@ -62,6 +65,74 @@ class _HomePageState extends State<HomePage> {
           )
         )
       )
+    );
+  }
+}
+
+
+class DataSearch extends SearchDelegate<String> {
+
+  final recentSearches = [
+    mealItemList.mealItems[1],
+    mealItemList.mealItems[6],
+    mealItemList.mealItems[3],
+  ];
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    //actions for app bar
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = "";
+        }),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    //leading icon on left of app bar
+    return IconButton(
+        icon: AnimatedIcon(
+          icon: AnimatedIcons.menu_arrow,
+          progress: transitionAnimation,
+        ),
+        onPressed: () {
+          close(context, null);
+        });
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    //show results based on selection
+    return Center();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    //show when user searches for something
+    final suggestionList = query.isEmpty
+        ? recentSearches
+        : mealItemList.mealItems;
+
+    return ListView.builder(
+      itemBuilder: (context, index){
+        if(suggestionList[index].mealName.contains(query)){
+          return ListTile(
+            onTap: () {Navigator.of(context).pushNamed('/MealProfilePage');},
+            leading: Icon(Icons.fastfood),
+            title: Text(
+              suggestionList[index].mealName,
+              style: Theme.of(context).textTheme.display1,
+            ),
+            subtitle: Text(suggestionList[index].restaurantName),);
+        }
+        else{
+          return Container(height: 0,);
+        }
+      },
+      itemCount: suggestionList.length,
     );
   }
 }
