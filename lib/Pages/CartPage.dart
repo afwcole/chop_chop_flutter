446f1f19d2b06f50fbeabeb188fdd5e55509de36
afwcole <afwcole@gmail.com>
2020-06-15 18:39:51 +0000

    Add Expansion Tiles for user to provide delivery and payment details.
    Add expansion tiles, one for the user to provide delivery information
    such as hostel(DropDown menu) and room number (TextField).
    another expansion tile for the user to choose which payment option
    (Cash/Wallet).

--- lib/Pages/CartPage.dart
+++ lib/Pages/CartPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'HomePage.dart';
 
@@ -12,14 +13,37 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
+  String mealName = "Beef Burger";
+  String restaurantName = "BBQ City";
+  double price = 15.99;
+  double subtotal = 49.99;
+  double deliveryFee = 1;
+  double total;
+  String deliveryLocation = "25B Brunei";
+
+  //Hostel dropdown variables
+  var _hostels = ["Brunei (Old)", "Brunei (New)", "Brunei (Complex)",
+    "Georgia", "SRC Hostel", "Hall 7", "Tek Credits", "Anglican Hostel"];
+  String _currentSelectedItem = "Brunei (Old)";
+  String _currentRoomInputted = "25B";
+  String _paymentMethod = "Cash";
+  bool _walletSelected = false;
+
+  void _onDropDownItemSelected(String valueSelected) {
+    setState(() {
+      this._currentSelectedItem = valueSelected;
+    });
+  }
+
+  void _onRoomNoSubmitted(String roomInputted) {
+    setState(() {
+      this._currentRoomInputted = roomInputted;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    String mealName = "Beef Burger";
-    String restaurantName = "BBQ City";
-    double price = 15.99;
-    double subtotal = 49.99;
-    double deliveryFee = 1;
-    double total = subtotal + deliveryFee;
+    total = subtotal + deliveryFee;
 
     return Scaffold(
       appBar: AppBar(
@@ -32,57 +56,251 @@ class _CartPageState extends State<CartPage> {
             SizedBox(height: 24),
             title("My", "Order"),
             SizedBox(height: 11),
-            cartMealTile(context, mealName, restaurantName, price, 5),
-            cartMealTile(context, mealName, restaurantName, price, 3),
-            cartMealTile(context, mealName, restaurantName, price, 1),
-            Divider(
-              color: Colors.grey,
-              thickness: 1,
-              indent: 24,
-              endIndent: 24,
+            ListView(
+              shrinkWrap: true,
+              physics: ClampingScrollPhysics(),
+              children: <Widget>[
+                cartMealTile(context, mealName, restaurantName, price, 5),
+                cartMealTile(context, mealName, restaurantName, price, 3),
+                cartMealTile(context, mealName, restaurantName, price, 1),
+              ],
             ),
-            feeRow(context, "Subtotal", subtotal, false),
-            SizedBox(height: 8),
-            feeRow(context, "Delivery Fee", deliveryFee, false),
-            SizedBox(height: 8),
-            feeRow(context, "Total", total, true),
             Divider(
-              color: Colors.grey,
-              thickness: 1,
-              indent: 24,
-              endIndent: 24,
-            ),
-            SizedBox(height: 74)
+              color: Colors.grey, thickness: 1, indent: 24, endIndent: 24,),
+            _feeRow("Subtotal", subtotal, false),
+            SizedBox(height: 8),
+            _feeRow("Delivery Fee", deliveryFee, false),
+            SizedBox(height: 8),
+            _feeRow("Total", total, true),
+            Divider(
+              color: Colors.grey, thickness: 1, indent: 24, endIndent: 24,),
+            _paymentExpansionSection(),
+            _deliveryExpansionSection(),
+            SizedBox(height: 74),
           ]),
           Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              margin: EdgeInsets.symmetric(vertical: 8, horizontal: 37),
-              width: MediaQuery.of(context).size.width,
-              child: RaisedButton(
-                padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-                color: Theme
-                    .of(context)
-                    .primaryColor,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                ),
-                onPressed: () {},
-                child: Text(
-                  "Checkout",
-                  style: Theme
-                      .of(context)
-                      .textTheme
-                      .button,
-                ),
-              ),
-            ),
+              alignment: Alignment.bottomCenter,
+              child: _checkoutButtonSection()
           ),
         ]),
       ),
     );
   }
+
+  Widget _paymentExpansionSection(){
+    return ExpansionTile(
+      leading: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: 6.0),
+            child: Text(
+              "Payment Method:",
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
+            ),
+          ),
+        ],
+      ),
+      title: Center(
+          child: Text(_paymentMethod, style: TextStyle(fontSize: 16),)
+      ),
+      children: <Widget>[
+        Container(
+          color: Colors.black12,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              ListTile(
+                contentPadding: EdgeInsets.fromLTRB(32, 8, 32, 0),
+                title: Text("Cash", style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.black),),
+                trailing: Icon(Icons.attach_money),
+                selected: !_walletSelected,//if wallet is not selected then cash is selected
+                onTap: (){
+                  setState(() {
+                    this._paymentMethod = "Cash";
+                    this._walletSelected = false;
+                  });
+                },
+                dense: true,
+              ),
+              Divider(),
+              ListTile(
+                contentPadding: EdgeInsets.fromLTRB(32, 0, 32, 0),
+                title: Text("Wallet", style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.black),),
+                trailing: Icon(Icons.account_balance_wallet),
+                selected: _walletSelected,
+                subtitle: Text("\$26.00"),
+                onTap: () {
+                  setState(() {
+                    this._paymentMethod = "Wallet";
+                    this._walletSelected = true;
+                  });
+                },
+                dense: true,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _deliveryExpansionSection(){
+    return ExpansionTile(
+      leading: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: 6.0),
+            child: Text(
+              "Deliver To:",
+              style: TextStyle(
+                  fontSize: 14, fontWeight: FontWeight.w600),),
+          ),
+        ],
+      ),
+      title: Center(
+          child: Text("$_currentRoomInputted $_currentSelectedItem",
+            style: TextStyle(fontSize: 16),)
+      ),
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(8),
+          color: Colors.black12,
+          child: Column(
+            children: <Widget>[
+              Container(
+                  padding: EdgeInsets.fromLTRB(32, 6, 6, 4),
+                  child: Row(
+                    children: <Widget>[
+                      Text("Hostel:", style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black),),
+                      SizedBox(width: 32),
+                      DropdownButton<String>(
+                        items: _hostels.map((String dropDownStringItem) {
+                          return DropdownMenuItem<String>(
+                            value: dropDownStringItem,
+                            child: Text(dropDownStringItem,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 16,
+                              ),
+                            ),
+                          );
+                        }).toList(),
+                        onChanged: (String valueSelected) {
+                          //Code to execute when menu item is selected from dropdown
+                          _onDropDownItemSelected(valueSelected);
+                        },
+                        value: _currentSelectedItem,
+                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color: Colors.black),
+                      )
+                    ],
+                  )
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(32, 8, 8, 8),
+                child: Row(
+                  children: <Widget>[
+                    Text("Room No:", style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.black),),
+                    SizedBox(width: 8),
+                    Container(
+                      width: 50,
+                      child: TextField(
+                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                        onSubmitted: (String roomInputted) {
+                          _onRoomNoSubmitted(roomInputted);
+                        },
+                        decoration: InputDecoration(
+                            isDense: true,
+                            hintText: "123Z"
+                        ),
+                        textCapitalization: TextCapitalization.characters,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _checkoutButtonSection(){
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 37),
+      width: MediaQuery
+          .of(context)
+          .size
+          .width,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          gradient: LinearGradient(
+              begin: Alignment.center,
+              end: Alignment.topCenter,
+              colors: <Color>[Color(0xFFFFFFEE), Color(0x00FFFFEE)])),
+      child: RaisedButton(
+        padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+        color: Theme
+            .of(context)
+            .primaryColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        onPressed: () {},
+        child: Text(
+          "Checkout",
+          style: Theme
+              .of(context)
+              .textTheme
+              .button,
+        ),
+      ),
+    );
+  }
+
+  Widget _feeRow(String amountName, double amount,
+      bool bold) {
+    final ThemeData themeStyle = Theme.of(context);
+
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 37),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(
+            amountName,
+            style: !bold
+                ? themeStyle.textTheme.body1
+                : themeStyle.textTheme.body1
+                .copyWith(fontWeight: FontWeight.w700, fontSize: 16),
+          ),
+          Text(
+            "\$$amount",
+            style: !bold
+                ? themeStyle.textTheme.body1
+                : themeStyle.textTheme.body1
+                .copyWith(fontWeight: FontWeight.w700, fontSize: 16),
+          ),
+        ],
+      ),
+    );
+  }
 }
+
 
 Widget cartMealTile(BuildContext context, String mealName,
     String restaurantName, double price, int qty) {
@@ -146,36 +364,8 @@ Widget cartMealTile(BuildContext context, String mealName,
           child: Text(
             "\$$price",
             textDirection: TextDirection.rtl,
-            style: Theme.of(context).textTheme.subtitle,
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
           ),
-        ),
-      ],
-    ),
-  );
-}
-
-Widget feeRow(
-    BuildContext context, String amountName, double amount, bool bold) {
-  final ThemeData themeStyle = Theme.of(context);
-
-  return Container(
-    margin: EdgeInsets.symmetric(horizontal: 37),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        Text(
-          amountName,
-          style: !bold
-              ? themeStyle.textTheme.body1
-              : themeStyle.textTheme.body1
-                  .copyWith(fontWeight: FontWeight.w700),
-        ),
-        Text(
-          "\$$amount",
-          style: !bold
-              ? themeStyle.textTheme.body1
-              : themeStyle.textTheme.body1
-                  .copyWith(fontWeight: FontWeight.w700),
         ),
       ],
     ),
