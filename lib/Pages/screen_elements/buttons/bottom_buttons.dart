303363b0f4443911008012cd8f271e907b7dea96
afwcole <afwcole@gmail.com>
2020-09-19 03:08:42 +0000

    Refactoring MealProfilePage by seperating the BottomButtons code into its own file and class.

---
+++ lib/Pages/screen_elements/buttons/bottom_buttons.dart
@@ -0,0 +1,137 @@
+import 'package:chop_chop_flutter/data_model/cart_item.dart';
+import 'package:chop_chop_flutter/data_model/extras_item.dart';
+import 'package:chop_chop_flutter/data_model/meal_item.dart';
+import 'package:chop_chop_flutter/providers/cart_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class BottomButtons extends StatefulWidget {
+  final MealItem mealItem;
+  final CartItem cartItem;
+  final List<ExtrasItem> selectedExtras;
+  final double currentMealBasePrice;
+
+  BottomButtons(
+      {Key key,
+      this.mealItem,
+      this.cartItem,
+      this.selectedExtras,
+      this.currentMealBasePrice})
+      : super(key: key);
+
+  @override
+  _BottomButtonsState createState() => _BottomButtonsState();
+}
+
+class _BottomButtonsState extends State<BottomButtons> {
+  MealItem _mealItem;
+  int _quantity;
+  double currentMealPrice;
+
+  @override
+  void initState() {
+    if (widget.mealItem != null) {
+      _mealItem = widget.mealItem;
+      _quantity = 1;
+    } else {
+      _mealItem = widget.cartItem.mealItem;
+      _quantity = widget.cartItem.quantity;
+    }
+    currentMealPrice = widget.currentMealBasePrice * _quantity;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(bottom: 8),
+      child: Row(
+        children: <Widget>[
+          SizedBox(width: 8),
+          twoWayButton(context),
+          SizedBox(width: 4),
+          Expanded(child: addToCartButton(context)),
+          SizedBox(width: 8),
+        ],
+      ),
+    );
+  }
+
+  Widget twoWayButton(BuildContext context) {
+    return Container(
+      width: 114,
+      padding: EdgeInsets.symmetric(vertical: 2),
+      decoration: BoxDecoration(
+          color: Color(0xFFCCCCCC), borderRadius: BorderRadius.circular(30)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          IconButton(
+              icon: Icon(Icons.remove, color: Colors.black, size: 16),
+              onPressed: () {
+                setState(() {
+                  if (_quantity > 1) {
+                    _quantity--;
+                  }
+                });
+              }),
+          Text(
+            "$_quantity",
+            style: Theme.of(context)
+                .textTheme
+                .button
+                .copyWith(color: Colors.black),
+          ),
+          IconButton(
+              icon: Icon(Icons.add, color: Colors.black, size: 16),
+              onPressed: () {
+                setState(() {
+                  _quantity++;
+                });
+              }),
+        ],
+      ),
+    );
+  }
+
+  Widget addToCartButton(BuildContext context) {
+    CartProvider _cartProvider = Provider.of<CartProvider>(context);
+    bool isEdit = widget.cartItem != null;
+    String buttonText = isEdit ? "Update" : "Add";
+    currentMealPrice = widget.currentMealBasePrice * _quantity;
+
+    return MaterialButton(
+      padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+      color: Theme.of(context).primaryColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+      onPressed: () {
+        CartItem finalCartItem = CartItem(
+          mealItem: _mealItem,
+          selectedExtras: widget.selectedExtras,
+          quantity: _quantity,
+          totalMealPrice: currentMealPrice,
+        );
+
+        if (_cartProvider.safeToAdd(finalCartItem)) {
+          if(isEdit){ //removes old cart item from the cart first
+            _cartProvider.removeFromCartList(widget.cartItem);
+          }
+
+          _cartProvider.addToCartList(finalCartItem);
+          Navigator.of(context).pop();
+        } else {
+          Scaffold.of(context).showSnackBar(new SnackBar(
+            content: Text("This item is already in the cart!"),
+            duration: Duration(milliseconds: 1500),
+          ));
+        }
+      },
+      child: Text(
+        "$buttonText  \$${currentMealPrice.toStringAsFixed(2)}",
+        style: Theme.of(context).textTheme.button,
+      ),
+    );
+  }
+}
--- lib/pages/meal_profile_page.dart
+++ lib/pages/meal_profile_page.dart
@@ -1,21 +1,18 @@
-import 'dart:async';
 import 'dart:ui';
 
 import 'package:chop_chop_flutter/data_model/cart_item.dart';
 import 'package:chop_chop_flutter/data_model/extras_item.dart';
 import 'package:chop_chop_flutter/data_model/meal_item.dart';
-import 'package:chop_chop_flutter/pages/screen_elements/buttons/cart_fab.dart';
+import 'package:chop_chop_flutter/pages/screen_elements/buttons/bottom_buttons.dart';
 import 'package:chop_chop_flutter/pages/screen_elements/buttons/pop_arrow_button.dart';
 import 'package:chop_chop_flutter/pages/screen_elements/display_restaurant_info.dart';
 import 'package:chop_chop_flutter/pages/screen_elements/header_and_logo.dart';
 import 'package:chop_chop_flutter/pages/screen_elements/my_custom_draggable_sheet.dart';
 import 'package:chop_chop_flutter/pages/screen_elements/tiles/checkbox_extras_tile.dart';
-import 'package:chop_chop_flutter/providers/cart_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/widgets.dart';
 import 'package:grouped_buttons/grouped_buttons.dart';
-import 'package:provider/provider.dart';
 
 class MealProfilePage extends StatefulWidget {
   final MealItem mealItem;
@@ -31,31 +28,30 @@ class MealProfilePage extends StatefulWidget {
 class _MealProfilePageState extends State<MealProfilePage> {
   MealItem _mealItem;
   List<ExtrasItem> _selectedExtras;
-  double _totalMealPrice;
-  int _quantity;
+  double _currentMealBasePrice;
   double _mealBasePrice;
   double _totalExtrasPrice;
-
-  int _buttonState = 0;
 
   @override
   void initState() {
     if (widget.mealItem != null) {
       _mealItem = widget.mealItem;
       _selectedExtras = <ExtrasItem>[];
-      _quantity = 1;
       _totalExtrasPrice = 0;
       _mealBasePrice = _mealItem.basePrice;
     } else {
       _mealItem = widget.cartItem.mealItem;
       _selectedExtras = widget.cartItem.selectedExtras;
-      _quantity = widget.cartItem.quantity;
       _totalExtrasPrice = widget.cartItem.getTotalExtrasPrice();
       _mealBasePrice = widget.cartItem.mealItem.basePrice;
     }
-    _totalMealPrice = (_mealBasePrice + _totalExtrasPrice) * _quantity;
+    _currentMealBasePrice = _mealBasePrice + _totalExtrasPrice;
     super.initState();
   }
+
+  //Getters
+  double get currentMealBasePrice => _currentMealBasePrice;
+  List<ExtrasItem> get selectedExtras => _selectedExtras;
 
   @override
   Widget build(BuildContext context) {
@@ -89,7 +85,9 @@ class _MealProfilePageState extends State<MealProfilePage> {
                             .copyWith(decoration: TextDecoration.underline)),
                   ),
                   CheckboxGroup(
-                    checked: getExtrasNameList(_selectedExtras),
+                    checked: _selectedExtras
+                        .map((extraItem) => extraItem.name)
+                        .toList(),
                     activeColor: Theme.of(context).primaryColor,
                     onChange: ((bool isChecked, String label, int index) {
                       setState(() {
@@ -101,8 +99,8 @@ class _MealProfilePageState extends State<MealProfilePage> {
                               _mealItem.extrasList[index].price;
                         }
 
-                        _totalMealPrice =
-                            (_mealBasePrice + _totalExtrasPrice) * _quantity;
+                        _currentMealBasePrice =
+                            _mealBasePrice + _totalExtrasPrice;
                       });
                     }),
                     onSelected: ((List<String> checked) {
@@ -124,188 +122,29 @@ class _MealProfilePageState extends State<MealProfilePage> {
             ),
           ],
         ),
-        floatingActionButton: Builder(
-          builder: (context) => bottomButtons(context),
-        ),
+        floatingActionButton: widget.mealItem != null
+            ? BottomButtons(
+                mealItem: _mealItem,
+                selectedExtras: selectedExtras,
+                currentMealBasePrice: currentMealBasePrice,
+              )
+            : BottomButtons(
+                cartItem: widget.cartItem,
+                selectedExtras: selectedExtras,
+                currentMealBasePrice: currentMealBasePrice,
+              ),
         floatingActionButtonLocation:
             FloatingActionButtonLocation.miniCenterDocked,
       ),
     );
   }
 
-  List<String> getExtrasNameList(List<ExtrasItem> selectedExtras) {
-    List<String> nameList = [];
-    for (ExtrasItem extraItem in selectedExtras) {
-      nameList.add(extraItem.name);
-    }
-    return nameList;
-  }
-
-  Widget bottomButtons(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(bottom: 8),
-      child: Row(
-        children: <Widget>[
-          SizedBox(width: 8),
-          twoWayButton(context),
-          SizedBox(width: 4),
-          Expanded(child: addButton(context)),
-          SizedBox(width: 8),
-        ],
-      ),
-    );
-  }
-
-  Widget twoWayButton(BuildContext context) {
-    return Container(
-      width: 114,
-      padding: EdgeInsets.symmetric(vertical: 2),
-      decoration: BoxDecoration(
-          color: Color(0xFFCCCCCC), borderRadius: BorderRadius.circular(30)),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          IconButton(
-              icon: Icon(Icons.remove, color: Colors.black, size: 16),
-              onPressed: () {
-                setState(() {
-                  if (_quantity != 0) {
-                    _quantity--;
-                    _totalMealPrice =
-                        (_mealBasePrice + _totalExtrasPrice) * _quantity;
-                  }
-                });
-              }),
-          Text(
-            "$_quantity",
-            style: Theme.of(context)
-                .textTheme
-                .button
-                .copyWith(color: Colors.black),
-          ),
-          IconButton(
-              icon: Icon(Icons.add, color: Colors.black, size: 16),
-              onPressed: () {
-                setState(() {
-                  _quantity++;
-                  _totalMealPrice =
-                      (_mealBasePrice + _totalExtrasPrice) * _quantity;
-                });
-              }),
-        ],
-      ),
-    );
-  }
-
-  Widget addButton(BuildContext context) {
-    var _cartProvider = Provider.of<CartProvider>(context);
-    bool isEdit = widget.cartItem != null;
-    String buttonText = isEdit ? "Update" : "Add";
-    CartItem finalCartItem;
-
-    return MaterialButton(
-      padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-      color: Theme.of(context).primaryColor,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(30.0),
-      ),
-      onPressed: () {
-        finalCartItem = CartItem(
-          mealItem: _mealItem,
-          selectedExtras: _selectedExtras,
-          quantity: _quantity,
-          totalMealPrice: _totalMealPrice,
-        );
-
-        if (_buttonState == 0) {
-          setState(() {
-            _buttonState = 1;
-          });
-
-          if (_cartProvider.safeToAdd(finalCartItem)) {
-            _cartProvider.addToCartList(finalCartItem);
-            Timer(Duration(milliseconds: 1500), () {
-              setState(() {
-                _buttonState = 2;
-              });
-            });
-
-            Timer(Duration(milliseconds: 2000), () {
-              Navigator.of(context).pop();
-            });
-          } else {
-            Timer(Duration(milliseconds: 1500), () {
-              setState(() {
-                _buttonState = 3;
-              });
-
-              
-              Scaffold.of(context).showSnackBar(new SnackBar(
-                content: Text("This item is already in the cart!"),
-                duration: Duration(milliseconds: 1500),
-              ));
-            });
-
-            Timer(Duration(milliseconds: 4500), () {
-              setState(() {
-                _buttonState = 0;
-              });
-            });
-          }
-        }
-      },
-      child: setupAddButtonChild(buttonText),
-    );
-  }
-
-  Widget setupAddButtonChild(String buttonText) {
-    if (_buttonState == 0) {
-      return new Text(
-        "$buttonText  \$${_totalMealPrice.toStringAsFixed(2)}",
-        style: Theme.of(context).textTheme.button,
-      );
-    } else if (_buttonState == 1) {
-      return SizedBox(
-        height: 20,
-        width: 20,
-        child: CircularProgressIndicator(
-          valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-        ),
-      );
-    } else if (_buttonState == 2) {
-      return Icon(
-        Icons.check,
-        color: Colors.white,
-        size: 20,
-      );
-    } else {
-      return Icon(
-        Icons.close,
-        color: Colors.white,
-        size: 20,
-      );
-    }
-  }
-
-  void animateButton() {
-    setState(() {
-      _buttonState = 1;
-    });
-
-    Timer(Duration(milliseconds: 3000), () {
-      setState(() {
-        _buttonState = 2;
-      });
-    });
-  }
-
   List<ExtrasItem> _getSelectedExtras(List<String> checked) {
     List<ExtrasItem> temp = [];
-    for (var i = 0; i < _mealItem.extrasList.length; i++) {
-      if (checked.contains(_mealItem.extrasList[i].name)) {
+    for (var i = 0; i < _mealItem.extrasList.length; i++)
+      if (checked.contains(_mealItem.extrasList[i].name))
         temp.add(_mealItem.extrasList[i]);
-      }
-    }
+
     return temp;
   }
 }
